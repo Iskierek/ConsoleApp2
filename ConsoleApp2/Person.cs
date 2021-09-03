c2998bc153f2a350d084ca3e520cd5dd88ade3b7
Michał Dutkowski <michal.dutkowski@crayon.com>
2021-09-03 13:28:45 +0200

    Add project files.

---
+++ ConsoleApp2/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ namespace ConsoleApp2
+{
+     public class Person
+    {
+        static int wzrost = 180;
+        static string imie ="Michal Jan pierwszy najlepszy", nazwisko;
+        static char litera ='c';
+
+
+
+        static void WalkForward() { 
+        
+        // IDz do przodu
+        }
+
+        public static void WalkBAckword()
+        { 
+            int dlugoscnogi = 50;
+            int numberofsteps = 20;
+
+            Console.WriteLine(dlugoscnogi * numberofsteps);
+     
+        }
+
+        public static void Talk()
+        {
+       
+        }
+
+        public static void SayYouName() {
+
+            Console.WriteLine(imie);
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using ConsoleApp2.NewFolder;
+using System;
+using System.Threading;
+
+
+namespace ConsoleApp2
+{
+   public class Program
+    {
+
+        int number = 5;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!222");
+
+            SecondClass.WriteAsd();
+
+            Person.WalkBAckword();
+
+        
+        }
+    }
+}
