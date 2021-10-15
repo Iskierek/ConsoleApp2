43371ac15bb65dcca01aff8c2d52ea989c8fec6c
Michal Dutkowski <michal.dutkowski@crayon.com>
2021-10-15 13:56:09 +0200

    loops

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -15,53 +15,13 @@ namespace ConsoleApp2
 
         static void Main(string[] args)
         {
-
-            List<int> ListaCen = new List<int>() { 120, 6, 1, 778, -2, 34, 23464576, 43534, 5456, 456, 54, 3456, -10, 0, -50 }; // 15
-
-            var iloscbledow = 0;
-            for (int i = 0; i < ListaCen.Count(); i++)
-            {
-
-                if (ListaCen[i] > 0)
-                {
-                    Console.WriteLine("OK: {0}", ListaCen[i]);
-                }
-                else
-                {
-                    Console.WriteLine("ZLE: {0}", ListaCen[i]);
-                    iloscbledow++;
-                }
-            }
-
-            Console.WriteLine("-------------");
-
-            int z = 0;
-
-            while (iloscbledow < 100) 
-            {
-
-                if (ListaCen[z] > 0)
-                {
-                    Console.WriteLine("OK: {0}", ListaCen[z]);
-                }
-                else
-                {
-                    Console.WriteLine("ZLE: {0}", ListaCen[z]);
-                    iloscbledow++;
-                }
-
-
-           
-            }
-
-
-            Console.WriteLine("ILOSC BLEDNYCH CEN WYNOSI: {0}", iloscbledow);
-            //RECURRENCE
-
-
-
-
+          //  loops.forLoop();
+          //  loops.advancedloops();
+            loops.whileloop();
         }
+
+
+      
 
 
     }
---
+++ ConsoleApp2/loops.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+   public class loops
+    {
+
+        public static void forLoop() {
+
+
+            for  (int i=0; i>=10; i++)
+            {
+                Console.WriteLine("Odpalilo sie: "+i);
+            
+            }
+
+
+        }
+
+
+        public static void advancedloops()
+        {
+
+            List<int> ListaCen = new List<int>() { 120, 6, 1, 778, -2, 34, 23464576, 43534, 5456, 456, 54, 3456, -10, 0, -50 }; // 15
+
+            Console.Out.WriteLine("Ilosc cen w liscie wynosi: "+ListaCen.Count());
+            var iloscbledow = 0;
+            for (int i = 0; i < ListaCen.Count(); i++)
+            {
+                if (ListaCen[i] > 0)
+                {
+
+                    Console.WriteLine("OK: {0}", ListaCen[i]);
+                }
+                else
+                {
+                    Console.WriteLine("ZLE: {0}", ListaCen[i]);
+                    iloscbledow++;
+        
+                }
+            }
+
+            Console.WriteLine("-------------");
+
+         
+
+
+            Console.WriteLine("ILOSC BLEDNYCH CEN WYNOSI: {0}", iloscbledow);
+            //RECURRENCE
+
+
+
+
+        }
+
+
+        public static void whileloop() {
+            List<int> ListaCen = new List<int>() { 120, 6, 1, 778, -2, 34, 23464576, 43534, 5456, 456, 54, 3456, -10, 0, -50 }; // 15
+            int i = 0;
+            var iloscbledow = 0;
+
+
+            while (iloscbledow < 2)
+            {
+
+                if (ListaCen[i] > 0)
+                {
+                    Console.WriteLine("OK: {0}", ListaCen[i]);
+                }
+                else
+                {
+                    Console.WriteLine("ZLE: {0}", ListaCen[i]);
+                    iloscbledow++;
+                }
+
+
+                i++;
+            }
+        }
+    }
+}
